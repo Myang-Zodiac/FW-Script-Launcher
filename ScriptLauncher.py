fd5f258f8c1b96594b15f55383489568e7b65876
Nimish Malla <Nimish.Malla@zodiac.com>
2021-07-15 10:44:51 -0700

    added settings page

--- ScriptLauncher.py
+++ ScriptLauncher.py
@@ -1,7 +1,7 @@
 #import PyQt5
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTabWidget, QDesktopWidget, QLabel, QListWidget, QPushButton, QTextEdit, QFileDialog, QMessageBox
-from PyQt5.QtGui import QIcon, QFont#, QFontDatabase
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTabWidget, QDesktopWidget, QLabel, QListWidget, QPushButton, QTextEdit, QFileDialog, QMessageBox, QLineEdit
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt, QSize
 import sys
 import os
@@ -15,11 +15,12 @@ class Window(QWidget):
         # Set window layout
 
         self.setInitUI()
+        self.DLPath = ''
 
         # Create dummy layout (temp solution)
         topLayout = QVBoxLayout()
         self.setLayout(topLayout)
-        self.optionsPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Tests')
+        self.optionsPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tests')
         # Create tab switcher widget
         self.tabWrapper = QTabWidget()
         self.tabWrapper.setFont(QFont('Calibri', 11))
@@ -28,6 +29,7 @@ class Window(QWidget):
         # Set up pages
         self.makeStartPage()
         self.makeResultsPage()
+        self.makeSettingsPage()
         # Add tab switcher widget to dummy layout
         topLayout.addChildWidget(self.tabWrapper)
 
@@ -42,7 +44,7 @@ class Window(QWidget):
         self.setFixedWidth(750)
         self.setFixedHeight(500)
         self.setWindowTitle("Script Launcher")
-        self.setWindowIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icon.png')))
+        self.setWindowIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets/icon.png')))
         self._center()
 
     def makeStartPage(self): # Called by __init__ 
@@ -59,10 +61,18 @@ class Window(QWidget):
         self.tabWrapper.addTab(self.startPage, 'Script Selection')
 
     def makeResultsPage(self): # Called by __init__ 
-        ''' Set up Results Page'''
+        '''Set up Results Page'''
         self.resultsPage = QWidget()
 
         self.tabWrapper.addTab(self.resultsPage, 'Results')
+
+    def makeSettingsPage(self):
+        '''Set up Settings Page'''
+        self.settingsPage = QWidget()
+
+        self._makePathInput()
+
+        self.tabWrapper.addTab(self.settingsPage, 'Settings')
     
     '''SUB-FUNCTIONS'''
     def _switchPage(self, targetPage): # General purpose tool function 
@@ -95,30 +105,37 @@ class Window(QWidget):
         self.submitTest.clicked.connect(self.__launchScriptHandler)
         self.listWidget.itemDoubleClicked.connect(self.__launchScriptHandler)
     def _makeSnippetView(self): # Called by makeStartPage 
+        '''Creates small sub-window to view info files for tests'''
+        # Explain how to show info snippet
         self.snippetInstructions = QLabel('Select a script to see its description', self.startPage)
         self.snippetInstructions.resize(350,20)
         self.snippetInstructions.move(375,15)
         self.snippetInstructions.setAlignment(Qt.AlignCenter)
-
+        # Set up window to view snippet
         self.scriptSnippet = QTextEdit(self.startPage)
         self.scriptSnippet.setReadOnly(True)
         self.scriptSnippet.setStyleSheet("border: 1px solid gray;")
         self.scriptSnippet.resize(350,365)
         self.scriptSnippet.move(375,50)
-        
+        # Update snippet shown on single-click
         self.listWidget.itemClicked.connect(self.__updateSnippet)
-    def _makeAddSubButtons(self):
+    def _makeAddSubButtons(self): # Called by makeStartPage
+        '''Creates add, subtract, folder, and refresh buttons'''
+        # Create folder button
         self.folderView = QPushButton(self.startPage)
         self.folderView.resize(35,35)
         self.folderView.move(262, 381)
-        self.folderView.setIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'foldericon.png')))
+        self.folderView.setIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets/foldericon.png')))
         self.folderView.setIconSize(QtCore.QSize(25,25))
+        # Create subtract button
         self.subScript = QPushButton('-', self.startPage)
         self.subScript.resize(35,35)
         self.subScript.move(294,381)
+        # Create add button
         self.addScript = QPushButton('+', self.startPage)
         self.addScript.resize(35,35)
         self.addScript.move(326,381)
+        # Create refresh button
         self.refresh = QPushButton(self.startPage)
         self.refresh.resize(27,27)
         self.refresh.move(333,23) #332 51
@@ -137,26 +154,43 @@ class Window(QWidget):
                                    "background-color : rgb(229,243,255);"
                                    "border : 0px"
                                    "}")
-        self.refresh.setIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'refreshicon.png')))
-
-
+        self.refresh.setIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets/refreshicon.png')))
+        # Enable actions on single-click
         self.folderView.clicked.connect(self.__folderView)
         self.subScript.clicked.connect(self.__subFile)
         self.addScript.clicked.connect(self.__addFile)
         self.refresh.clicked.connect(self.__refresh)
+    def _makePathInput(self): # Called by makeSettingsPage
+        '''Creates input box to get Docklight path if not already saved'''
+        # Create input box
+        self.pathInput = QLineEdit(self.settingsPage)
+        self.pathInput.resize(600,25)
+        self.pathInput.move(15,15)
+        # Ready storage file for reading
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets/dlpath.txt'), "r+") as f:
+            txt = f.read().rstrip()
+            if not os.path.isfile(txt) or not txt.endswith("Docklight_Scripting.exe"):
+                self.pathInput.setPlaceholderText('Input DockLight Scripting path')
+            else:
+                self.pathInput.setPlaceholderText('Valid path entered')
+                self.pathInput.setText(txt)
+        # Create save button
+        self.pathInputButton = QPushButton('Save', self.settingsPage)
+        self.pathInputButton.setFont(QFont('Calibri', 9))
+        self.pathInputButton.resize(100,25)
+        self.pathInputButton.move(620, 15)
+        # Call function to save data
+        self.pathInputButton.clicked.connect(self.__handlePath)
+
+
 
     '''SUB-SUB-FUNCTIONS'''
     def __getOptions(self): # Called by _makeScriptList 
         '''Retrieves available tests and creates a dictionary with name:path pairs'''
-        #appPath = os.path.dirname(os.path.abspath(__file__))
         self.options = {}
         folder = os.listdir(self.optionsPath)
         for test in folder:
-            #self.optionPaths.append(os.path.join(optionsPath, test))
             self.options[test] = os.path.join(self.optionsPath, test)
-        #for optionPath in self.optionPaths:
-            #self.options.append(optionPath.split('\\')[-1])
-
     def __updateSnippet(self, item): # Called by _makeSnippetView on event 
         '''Verifies whether info.txt exists and displays info/error accordingly'''
         try:
@@ -168,7 +202,6 @@ class Window(QWidget):
         except:
             self.scriptSnippet.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
             self.scriptSnippet.setPlainText('Error: no info file found')
-    
     def __launchScriptHandler(self, item): # Called by _makeScriptList on events 
         '''Verifies whether selected script exists and launches or displays error accordingly'''
         self._switchPage(self.resultsPage)
@@ -182,21 +215,17 @@ class Window(QWidget):
             pass
         else: # display success message
             pass
-    
-    #'''
-    def __folderView(self):
-        #os.system(f'explorer {self.optionsPath}')
+    def __folderView(self): # Called by _makeAddSubButtons
+        '''Opens file explorer in options path'''
         subprocess.Popen(f'explorer {self.optionsPath}', shell=True)
-    #'''
-
-    def __addFile(self):
+    def __addFile(self): # Called by _makeAddSubButtons
+        '''Adds selected folder to directory'''
         folder = QFileDialog.getExistingDirectory(self, 'Select Folder')
         if folder == '':
             return
         try:
             os.mkdir(os.path.join(self.optionsPath, folder.split('/')[-1]))
         except FileExistsError as e:
-            print(e)
             msg = QMessageBox(self)
             msg.setWindowTitle("Error")
             msg.setText("The directory already exists")
@@ -208,8 +237,8 @@ class Window(QWidget):
             copy_tree(folder, os.path.join(self.optionsPath, folder.split('/')[-1]))
             self.listWidget.addItem(folder.split('/')[-1])
             self.__getOptions()
-    
-    def __subFile(self):
+    def __subFile(self): # Called by _makeAddSubButtons
+        '''Removes selected folder from directory'''
         if len(self.listWidget.selectedItems()) == 1:
             path = self.options[self.listWidget.selectedItems()[0].text()]
             shutil.rmtree(path)
@@ -224,18 +253,28 @@ class Window(QWidget):
             msg.setStandardButtons(QMessageBox.Ok)
             msg.setDefaultButton(QMessageBox.Ok)
             msg.exec()
-    
-    def __refresh(self):
+    def __refresh(self): # Called by _makeAddSubButtons
+        '''Refreshes script list'''
         optionsTemp = self.options.keys()
         self.__getOptions()
         for opt in self.options:
             if opt not in optionsTemp:
                 self.listWidget.addItem(opt)
+    def __handlePath(self): # Called by _makePathInput
+        '''Verifies Docklight path when adding new path'''
+        if self.DLPath == '':
+            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets/dlpath.txt'), "r+") as f:
+                if os.path.isfile(self.pathInput.text()) and self.pathInput.text().endswith("Docklight_Scripting.exe"):
+                    f.truncate(0)
+                    f.write(self.pathInput.text())
+                    self.pathInput.setPlaceholderText('Valid path saved')
+                else:
+                    self.pathInput.setPlaceholderText('Invalid path. Please enter the correct path.')
+                    self.pathInput.clear()
         
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
-    #window.show()
     sys.exit(app.exec_())
